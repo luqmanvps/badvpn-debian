d5e4449d4bc95ed6d5b4bc56c82d0a50b94ff608
luqmanvps <67447668+luqmanvps@users.noreply.github.com>
2020-07-03 13:05:56 +0000

    Update badvpn.sh

--- badvpn.sh
+++ badvpn.sh
@@ -4,6 +4,6 @@ wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/Clrkz/VPSAutoSc
 if [ "$OS" == "x86_64" ]; then
   wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/Clrkz/VPSAutoScrptz/master/badvpn-udpgw64"
 fi
-sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300' /etc/rc.local --max-clients 500
+sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 /etc/rc.local --max-clients 500'
 chmod +x /usr/bin/badvpn-udpgw
 screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 500
